879db66881a5c895edc8028b0b85e649e0c857f5
imshivam-gupta <gupta.shivam2604@gmail.com>
2024-05-23 14:07:59 +0530

    connection added

--- crs-jedi-java-project-pos/src/com/flipkart/dao/AdminDaoInterface.java
+++ crs-jedi-java-project-pos/src/com/flipkart/dao/AdminDaoInterface.java
@@ -1,5 +1,44 @@
+/**
+ * 
+ */
 package com.flipkart.dao;
 
-public interface AdminDaoInterface {
+import java.util.List;
 
+import com.flipkart.bean.Course;
+import com.flipkart.bean.Professor;
+import com.flipkart.bean.Student;
+import com.flipkart.bean.User;
+
+
+/**
+ * @author JEDI-03
+ * Interface for Admin Dao Operations
+ *
+ */
+public interface AdminDaoInterface {
+	
+
+	public void deleteCourse(String courseCode);
+	
+	public void addCourse(Course course);
+
+	public List<Student> viewPendingAdmissions();
+	
+
+	public void approveStudent(int studentId);
+	
+
+	public void addProfessor(Professor professor);
+
+	public void addUser(User user);
+	
+
+	public void assignCourse(String courseCode, String professorId);
+	
+
+	public List<Course> viewCourses(int catalogId);
+	
+
+	public List<Professor> viewProfessors();
 }
