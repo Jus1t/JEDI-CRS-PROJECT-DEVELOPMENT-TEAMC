1636118aa31f213bb50c92e02d8ab4a4d8293b26
imshivam-gupta <gupta.shivam2604@gmail.com>
2024-05-25 10:06:55 +0530

    done proj

--- crs-jedi-java-project-pos/src/com/flipkart/utils/DBUtils.java
+++ crs-jedi-java-project-pos/src/com/flipkart/utils/DBUtils.java
@@ -1,6 +1,10 @@
 package com.flipkart.utils;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
 
 public class DBUtils {
 	static final String DB_URL = "jdbc:mysql://localhost/CRS-Schema";
@@ -9,14 +13,32 @@ public class DBUtils {
 
 	public static Connection getConnection() {
 		Connection conn = null;
-		try {
-			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		if (conn != null)
+			return conn;
+		else {
+			try {
+				Properties prop = new Properties();
+                InputStream inputStream = DBUtils.class.getClassLoader().getResourceAsStream("./config.properties");
+                prop.load(inputStream);
+                String url = prop.getProperty("db_url");
+                String user = prop.getProperty("user");
+                String password = prop.getProperty("pass");
+                
+				conn = DriverManager.getConnection(url, user, password);
+//				conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
-		} catch (SQLException e) {
-			e.printStackTrace();
+			}
+			 catch (SQLException e) {
+	                e.printStackTrace();
+	            }
+	            catch (FileNotFoundException e) {
+	                e.printStackTrace();
+	            }
+	            catch (IOException e) {
+	                e.printStackTrace();
+	            }
+			return conn;
 		}
-
-		return conn;
 
 	}
 
